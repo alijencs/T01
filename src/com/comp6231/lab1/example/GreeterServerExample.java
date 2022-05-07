f5fc0d005187fcc09298566f84f37d1acd2dd0b5
Pouria <pouriaroostaee.ce@gmail.com>
2022-05-07 10:49:44 -0400

    Update GreeterServerExample.java

--- src/com/comp6231/lab1/example/GreeterServerExample.java
+++ src/com/comp6231/lab1/example/GreeterServerExample.java
@@ -22,7 +22,7 @@ public class GreeterServerExample {
         try {
             ServerListener server = new ServerListener("GreeterServer", 6231, GreeterServerProtocol::new);
             server.start();
-            System.out.println("\nThe server is listening on port 431.\nPlease open telnet and connect to " +
+            System.out.println("\nThe server is listening on port 6231.\nPlease open telnet and connect to " +
                     "this machine on port 6231. You may open multiple simultaneous connections.");
             System.out.println("\nPress hit ENTER if you wish to stop the server. Note that the service may NOT stop immediately.");
             new Scanner(System.in).nextLine(); // waiting for EOL from the console to terminate
